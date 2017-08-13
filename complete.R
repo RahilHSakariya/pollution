941497dffa71ff9ad507a5056753eaed7bde3373
Pisanu <dev.pisanu@gmail.com>
2017-08-13 21:32:01 +0700

    Fixed incorrect result when input IDs are reverse order

--- complete.R
+++ complete.R
@@ -1,10 +1,20 @@
 complete <- function(directory, id = 1:332) {
     files <-  paste(directory, '/', formatC(id, width = 3, format = "d", flag = "0"), '.csv', sep="")
-    tables <- lapply(files, read.csv, header = TRUE)
-    data <- do.call(rbind , tables)
+   
+    nobs <- vector(mode = 'numeric', length = length(files))
+    for (i in 1:length(files)) {
+        input = read.csv(files[i])
+        data <- input[!(is.na(input$nitrate) | is.na(input$sulfate)), ]
+        nobs[i] <- NROW(data)
+    }
+
+    data.frame(id, nobs)
     
-    data.complete = data[!(is.na(data$nitrate) | is.na(data$sulfate)), ]
-    result <- aggregate(Date~ID, data.complete, FUN=NROW)
-    names(result) <- c("id","nobs")
-    result
+    # tables <- lapply(files, read.csv, header = TRUE)
+    # data <- do.call(rbind , tables)
+    # 
+    # completes <- data[!(is.na(data$nitrate) | is.na(data$sulfate)), ]
+    # result <- aggregate(Date~ID, completes, FUN=NROW)
+    # names(result) <- c("id","nobs")
+    # result
 }
